03fabf8c0042c14985fea5d7d7190f8564c7befc
alinnert <andreas@linnertmedia.de>
2024-04-25 09:52:57 +0000

    Deploying to release from @ alinnert/leapwork-data-extractor@a9c0a4efa334e3edd0037b8e408545edbe0cfa19 🚀

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const inputEl = $id('input')
 const outputEl = $id('output')
 const linescountEl = $id('linescount')
 
+$id('clear-data-button').addEventListener('click', () => {
+  inputEl.value = ''
+  handleInputElInput([])
+})
+
+$id('paste-data-button').addEventListener('click', async () => {
+  inputEl.value = await navigator.clipboard.readText()
+  handleInputElInput(inputEl.value.split('\n'))
+})
+
+$id('copy-result-button').addEventListener('click', async () => {
+  if (outputEl.value === '') return
+  await navigator.clipboard.writeText(outputEl.value)
+})
+
 const regex =
   /Timestamp: (\d\d)\.(\d\d)\.(\d\d\d\d) (\d\d:\d\d:\d\d).*Flow: ([^,]+)/
 
@@ -10,11 +25,14 @@ const isResultLine = (l, r) =>
   l.endsWith(`Result: ${r}`) && l.match(regex) !== null
 
 inputEl.addEventListener('input', (event) => {
-  const lines = event.target.value.split('\n')
+  handleInputElInput(event.target.value.split('\n'))
+})
 
+function handleInputElInput(lines) {
   const successLines = lines.filter((l) => isResultLine(l, 'Passed'))
   const doneLines = lines.filter((l) => isResultLine(l, 'Done'))
   const failedLines = lines.filter((l) => isResultLine(l, 'Failed'))
+  const runningLines = lines.filter((l) => isResultLine(l, 'Running'))
 
   const extractedData = failedLines
     .map((l) => l.match(regex))
@@ -27,6 +45,7 @@ inputEl.addEventListener('input', (event) => {
   const success = successLines.length
   const done = doneLines.length
   const failed = failedLines.length
+  const running = runningLines.length
 
   const [, day, month, year] = extractedData[0] ?? []
   const date =
@@ -34,22 +53,23 @@ inputEl.addEventListener('input', (event) => {
       ? `${year}-${month}-${day}`
       : null
 
-  setLinescount({ date, success, done, failed })
-})
+  setLinescount({ date, success, done, failed, running })
+}
 
 function setOutput(items) {
   outputEl.value = items.join('\n')
 }
 
 function setLinescount(
-  { date, success, done, failed } = {
+  { date, success, done, failed, running } = {
     date: null,
     success: 0,
     done: 0,
     failed: 0,
+    running: 0,
   },
 ) {
-  const total = success + done + failed
+  const total = success + done + failed + running
 
   if (total === 0 || date === null) {
     linescountEl.innerHTML = `<div>Keine Testergebnisse vorhanden</div>`
@@ -59,30 +79,42 @@ function setLinescount(
   const successPercentage = (100 / total) * success
   const donePercentage = (100 / total) * done
   const failedPercentage = (100 / total) * failed
+  const runningPercentage = (100 / total) * running
 
   linescountEl.innerHTML = `
   <div class="total-count">
-    insgesamt: ${total}
+    ${total} Testläufe
   </div>
   <div class="failed-count">
-    fehlgeschlagen: ${failed} (${Math.round(failedPercentage)} %)
+    ${failed} fehlgeschlagen (${Math.round(failedPercentage)} %)
   </div>
   <div class="done-count">
-    beendet: ${done} (${Math.round(donePercentage)} %)
+    ${done} beendet (${Math.round(donePercentage)} %)
   </div>
   <div class="success-count">
-    erfolgreich: ${success} (${Math.round(successPercentage)} %)
+    ${success} erfolgreich (${Math.round(successPercentage)} %)
   </div>
-  <div class="progress" style="grid-template-columns: ${failed}fr ${done}fr ${success}fr;">
+  <div class="running-count">
+    ${running} laufend (${Math.round(runningPercentage)} %)
+  </div>
+  <div class="progress" style="grid-template-columns: ${failed}fr ${done}fr ${success}fr ${running}fr;">
     <div class="progress-failed"></div>
     <div class="progress-done"></div>
     <div class="progress-success"></div>
+    <div class="progress-running"></div>
   </div>
   <div class="statusbar-text">Confluence-Überschrift:</div>
-  <div class="confluence-template">
+  <div class="confluence-template" id="confluence-headline">
     ${date} ${Math.round(successPercentage)}% (${success}/${total})
   </div>
+  <div class="statusbar-button">
+    <button id="copy-confluence-headline-button">Kopieren</button>
+  </div>
   `
+
+  $id('copy-confluence-headline-button').addEventListener('click', async () => {
+    await navigator.clipboard.writeText($id('confluence-headline').textContent.trim())
+  })
 }
 
 setLinescount()
