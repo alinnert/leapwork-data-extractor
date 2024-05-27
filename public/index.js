0958d502152d1e201d8ca27eb989025542fd4dee
Andreas Linnert <andreas@linnertmedia.de>
2024-05-27 12:13:40 +0200

    Update line parser
    Support unknown lines.
    Show results when all tests have passed.
    Show crown when all tests have passed.

--- public/index.js
+++ public/index.js
@@ -29,16 +29,31 @@ inputEl.addEventListener('input', (event) => {
 })
 
 function handleInputElInput(lines) {
-  const successLines = lines.filter((l) => isResultLine(l, 'Passed'))
-  const doneLines = lines.filter((l) => isResultLine(l, 'Done'))
-  const failedLines = lines.filter((l) => isResultLine(l, 'Failed'))
-  const runningLines = lines.filter((l) => isResultLine(l, 'Running'))
+  let successLines = []
+  let doneLines = []
+  let failedLines = []
+  let runningLines = []
+  let unknownLines = []
+
+  for (const line of lines.filter(l => l.trim() !== '')) {
+    if (isResultLine(line, 'Passed')) {
+      successLines.push(line)
+    } else if (isResultLine(line, 'Done')) {
+      doneLines.push(line)
+    } else if (isResultLine(line, 'Failed')) {
+      failedLines.push(line)
+    } else if (isResultLine(line, 'Running')) {
+      runningLines.push(line)
+    } else {
+      unknownLines.push(line)
+    }
+  }
 
   const extractedData = failedLines
     .map((l) => l.match(regex))
     .filter((l) => l !== null)
 
-  const failedExcelData = extractedData.map((m) => m[4] + '\t' + m[5])
+  const failedExcelData = extractedData.map((m) => `${m[4]}\t${m[5]}`)
 
   setOutput(failedExcelData)
 
@@ -46,14 +61,21 @@ function handleInputElInput(lines) {
   const done = doneLines.length
   const failed = failedLines.length
   const running = runningLines.length
+  const unknown = unknownLines.length
 
-  const [, day, month, year] = extractedData[0] ?? []
+  const firstLineMatch = lines[0].match(regex)
+  if (firstLineMatch === null) {
+    setLinescount()
+    return
+  }
+
+  const [, day, month, year] = firstLineMatch ?? []
   const date =
     day !== undefined && month !== undefined && year !== undefined
       ? `${year}-${month}-${day}`
       : null
 
-  setLinescount({ date, success, done, failed, running })
+  setLinescount({ date, success, done, failed, running, unknown })
 }
 
 function setOutput(items) {
@@ -61,42 +83,64 @@ function setOutput(items) {
 }
 
 function setLinescount(
-  { date, success, done, failed, running } = {
+  { date, success, done, failed, running, unknown } = {
     date: null,
     success: 0,
     done: 0,
     failed: 0,
     running: 0,
+    unknown: 0,
   },
 ) {
-  const total = success + done + failed + running
+  const total = success + done + failed + running + unknown
 
   if (total === 0 || date === null) {
     linescountEl.innerHTML = `<div>Keine Testergebnisse vorhanden</div>`
     return
   }
 
-  const successPercentage = (100 / total) * success
-  const donePercentage = (100 / total) * done
-  const failedPercentage = (100 / total) * failed
-  const runningPercentage = (100 / total) * running
+  const successPercentage = getPercent(total, success)
+  const donePercentage = getPercent(total, done)
+  const failedPercentage = getPercent(total, failed)
+  const runningPercentage = getPercent(total, running)
+  const unknownPercentage = getPercent(total, unknown)
+
+  const failedCountHtml = `
+    <div class="failed-count">
+      ${failed} fehlgeschlagen (${failedPercentage})
+    </div>`
+
+  const doneCountHtml = `
+    <div class="done-count">
+      ${done} beendet (${donePercentage})
+    </div>`
+
+  const successCountHtml = `
+    <div class="success-count">
+      ${success} erfolgreich (${successPercentage})
+    </div>`
+
+  const runningCountHtml = `
+    <div class="running-count">
+      ${running} laufend (${runningPercentage})
+    </div>`
+  
+  const unknownCountHtml = `
+    <div class="unknown-count">
+      ${unknown} unbekannt (${unknownPercentage})
+    </div>`
+
+  const crown = success === total ? ' - 👑' : ''
 
   linescountEl.innerHTML = `
   <div class="total-count">
     ${total} Testläufe
   </div>
-  <div class="failed-count">
-    ${failed} fehlgeschlagen (${Math.round(failedPercentage)} %)
-  </div>
-  <div class="done-count">
-    ${done} beendet (${Math.round(donePercentage)} %)
-  </div>
-  <div class="success-count">
-    ${success} erfolgreich (${Math.round(successPercentage)} %)
-  </div>
-  <div class="running-count">
-    ${running} laufend (${Math.round(runningPercentage)} %)
-  </div>
+  ${failed > 0 ? failedCountHtml : ''}
+  ${done > 0 ? doneCountHtml : ''}
+  ${success > 0 ? successCountHtml : ''}
+  ${running > 0 ? runningCountHtml : ''}
+  ${unknown > 0 ? unknownCountHtml : ''}
   <div class="progress" style="grid-template-columns: ${failed}fr ${done}fr ${success}fr ${running}fr;">
     <div class="progress-failed"></div>
     <div class="progress-done"></div>
@@ -105,7 +149,7 @@ function setLinescount(
   </div>
   <div class="statusbar-text">Confluence-Überschrift:</div>
   <div class="confluence-template" id="confluence-headline">
-    ${date} ${Math.round(successPercentage)}% (${success}/${total})
+    ${date} ${successPercentage} (${success}/${total})${crown}
   </div>
   <div class="statusbar-button">
     <button id="copy-confluence-headline-button">Kopieren</button>
@@ -113,8 +157,15 @@ function setLinescount(
   `
 
   $id('copy-confluence-headline-button').addEventListener('click', async () => {
-    await navigator.clipboard.writeText($id('confluence-headline').textContent.trim())
+    await navigator.clipboard.writeText(
+      $id('confluence-headline').textContent.trim(),
+    )
   })
+}
+
+function getPercent(total, amount) {
+  const percent = Math.round((1000 / total) * amount) / 10
+  return `${percent}%`
 }
 
 setLinescount()
